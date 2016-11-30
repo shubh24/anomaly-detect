6dd7695d856253df3277b2c3b2a1b413cfc62b01
Gyanendra Mishra <anomaly.the@gmail.com>
2016-11-30 23:47:58 +0530

    standard deviation

---
+++ sd.py
@@ -0,0 +1,42 @@
+import csv
+import numpy as np
+from matplotlib import pyplot as plt
+
+h = []
+t = []
+
+with open("floor2.csv", "rb") as f:
+    for i in f.readlines():
+        h.append(float(i.split(",")[1]))
+        t.append(float(i.split(",")[0]))
+
+mean = sum(h) / len(h)
+
+np_array = np.array(h)
+
+std = np.std(np_array)
+
+plt.figure()
+
+plt.plot(t, h)
+
+plt.plot(t, [mean] * len(t))
+plt.plot(t, [mean + 2 * std] * len(t))
+plt.plot(t, [mean - 2 * std] * len(t))
+plt.plot(t, [mean + 3 * std] * len(t))
+plt.plot(t, [mean - 3 * std] * len(t))
+
+
+print 'outliers t vs v according to 3SD method'
+for index, value in enumerate(h):
+    if value < mean - 2 * std or value > mean + 2 * std:
+        print t[index], value
+
+
+print 'outliers t vs v according to 3SD method'
+for index, value in enumerate(h):
+    if value < mean - 3 * std or value > mean + 3 * std:
+        print t[index], value
+
+
+plt.show()
